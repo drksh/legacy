4e1e42b7e4bb959c21937814b32936f1a009cd98
Jakob Steinn <jakob.sturluson@gmail.com>
2015-02-08 19:00:49 +0100

    initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Less
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+var bowerPath = "vendor/bower_components";
+var resourcePath = "resources/assets";
+
+elixir(function(mix) {
+    mix.sass('app.less');
+
+    mix.copy(bowerPath+"/bootstrap-sass-official/assets/stylesheets", resourcePath+"/sass/bootstrap");
+});
